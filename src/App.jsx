3ec1dfe651ca37a41dd9c251876042411a4f8bd5
PedroDallaCosta <pedrosdc2022@gmail.com>
2025-07-05 14:28:55 -0300

    Fix useUserContext error app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
-import { UserProvider } from "@/context/UserContext";
+import { UserProvider } from "@/context/userContext";
 import { Toaster } from "@/components/ui/sonner"
 
 import Container from '@/components/Container';
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr()],
   resolve: {
